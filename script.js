a25e33c5929281bd49838b59dbf00e72b7f9583e
Mathew Ritchie <ritchmat89@gmail.com>
2025-01-10 19:53:11 +0200

    my project copied from Icodethis

---
+++ script.js
@@ -0,0 +1,10 @@
+let followBtn = document.getElementById("followBtn");
+let followBtn2 = document.getElementById("followBtn2");
+
+followBtn.addEventListener("click", function () {
+  followBtn.textContent = "Following";
+});
+
+followBtn2.addEventListener("click", function () {
+  followBtn2.textContent = "Following";
+});
